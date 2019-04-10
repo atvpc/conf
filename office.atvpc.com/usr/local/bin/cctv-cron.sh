9f192197239f9519ae43f6ec00542dbb7776bbb9
Timothy Keith <timothykeith@gmail.com>
2019-04-10 17:11:53 -0400

    fix to make sure entire video stream is buffered before grabbing a frame

--- office.atvpc.com/usr/local/bin/cctv-cron.sh
+++ office.atvpc.com/usr/local/bin/cctv-cron.sh
@@ -5,6 +5,6 @@ source /usr/local/bin/cctv-secrets.sh
 
 for i in $(seq 1 8); do
 	URI="rtsp://${USER}:${PASS}@${IP}:554/cam/realmonitor?channel=${i}&subtype=0"
-	ffmpeg -loglevel panic -y -i "$URI" -vframes 1 "/srv/htdocs/cctv/${i}.jpg";
+	ffmpeg -loglevel panic -rtsp_transport tcp -y -i "$URI" -vframes 1 "/srv/htdocs/cctv/${i}.jpg";
 	chown www-data:www-data -R /srv/htdocs/cctv
 done
